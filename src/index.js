27379ece30a67eeabbdf00ce903517a49fa9a07c
Sungmin Yi <ssuyi@ucdavis.edu>
2018-06-04 16:19:41 -0700

    updated libraries

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import reducers from './reducers';
 import Router from './router';
 
 import rootEpic from './epics';
-const epicMiddleware = createEpicMiddleware(rootEpic);
+const epicMiddleware = createEpicMiddleware();
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const history = createHistory()
@@ -27,6 +27,7 @@ const store = createStore(
     applyMiddleware(thunk,middleware,epicMiddleware)
   )
 )
+epicMiddleware.run(rootEpic);
 
 
 ReactDOM.render(
